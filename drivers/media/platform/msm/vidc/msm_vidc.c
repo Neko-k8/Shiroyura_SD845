UtsavBalar1231/kernel_xiaomi_sm8250

msm: vidc: do not allow queue buffer in flush
Client is not supposed to queue buffer while driver
is in flush. Return error if client queue buffer
in flush to avoid possible deadlock issues in driver
due to release buffer reference event processing.

Change-Id: Ica2ff40e428f82b9f6d3aa1314f7bf7d322375bd
Signed-off-by: Maheshwar Ajja <majja@codeaurora.org>

msm/vidc/msm_vidc.c
@@ -340,6 +340,12 @@ int msm_vidc_qbuf(void *instance, struct v4l2_buffer *b)
 		return -EINVAL;
 	}
 
+	if (inst->in_flush && is_decode_session(inst) &&
+		b->type == OUTPUT_MPLANE) {
+		dprintk(VIDC_ERR, "%s: in flush, discarding qbuf\n", __func__);
+		return -EINVAL;
+	}
+
 	for (i = 0; i < b->length; i++) {
 		b->m.planes[i].m.fd = b->m.planes[i].reserved[0];
 		b->m.planes[i].data_offset = b->m.planes[i].reserved[1];

Neko-k8/Shiroyura_SD845

drivers/media/platform/msm/vidc/msm_vidc.c
@@ -529,6 +529,11 @@ int msm_vidc_qbuf(void *instance, struct v4l2_buffer *b)
 	}
 	mutex_lock(&q->lock);
 
+	if (inst->in_flush && is_decode_session(inst) &&
+		b->type == V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE) {
+		dprintk(VIDC_ERR, "%s: in flush, discarding qbuf\n", __func__);
+		return -EINVAL;
+	}
 
 	for (i = 0; i < b->length; i++) {
 		b->m.planes[i].m.fd = b->m.planes[i].reserved[0];